2302dc64a3af9577220a293a4bdb03c59e383ec7
Vuhuuduy <duy3atv@gmail.com>
2024-11-19 20:09:21 +0700

    up db

---
+++ commons/env.php
@@ -0,0 +1,19 @@
+<?php
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+// Đường dẫn vào client
+
+define('BASE_URL', '');
+
+// Đường dẫn vào admin
+
+define('BASE_URL_ADMIN', '');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', '');
+define('DB_NAME', 'duan');  // Tên database
+
+define('PATH_ROOT', __DIR__ . '/../');
