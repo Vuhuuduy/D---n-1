c7bf5bb6a09582756983c94d2628a41a6d204411
Đặng Duy Tiến <tienddph50842@gmail.com>
2024-12-11 22:14:38 +0700

    update đổi mật khẩu , thông tin

--- commons/env.php
+++ commons/env.php
@@ -4,16 +4,16 @@
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 // Đường dẫn vào client
 
-define('BASE_URL', 'http://localhost/Du_an_1/Du_an1/');
+define('BASE_URL', 'http://localhost/Du_An1/');
 
 // Đường dẫn vào admin
 
-define('BASE_URL_ADMIN', 'http://localhost/Du_an_1/Du_an1/admin');
+define('BASE_URL_ADMIN', 'http://localhost/Du_An1/admin');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'duan2');  // Tên database
+define('DB_NAME', 'du_annn1');  // Tên database
 
 define('PATH_ROOT', __DIR__ . '/../');
